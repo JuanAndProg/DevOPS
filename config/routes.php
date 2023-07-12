17f889d25b6c830434fcfa9dbab3c99dc346c71a
JuanAndProg <130148119+JuanAndProg@users.noreply.github.com>
2023-07-12 12:00:16 +0200

    Web Scheme & Routes (#2)
    * Update routes.php
    
    Initiate project, first step define routes
    
    * Add files via upload
    
    * test for efficiency working flow style
    
    * test for pull request on flow_style
    
    * correcting slash and defining definitive routes

--- config/routes.php
+++ config/routes.php
@@ -1,14 +1,14 @@
 <?php 
 
 /**
- * Used to define the routes in the system.
- * 
- * A route should be defined with a key matching the URL and an
- * controller#action-to-call method. E.g.:
- * 
- * '/' => 'index#index',
- * '/calendar' => 'calendar#index'
+ * delete test 
+ * start working on application
  */
 $routes = array(
-	'/test' => 'test#index'
+	'/home' => 'application#showList',
+	'/create'=> 'application#createTask',
+	'/view'=> 'application#viewTask',
+	'/edit'=> 'application#editTask',
+	'/delete'=>'application#deleteTask'
+	
 );
